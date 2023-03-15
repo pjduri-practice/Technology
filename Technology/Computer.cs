081a929a56aacb369c62c5116ac4badc51aa3e34
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-15 14:12:33 -0500

    Add project files.

---
+++ Technology/Computer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technology
+{
+    public class Computer
+    {
+        public double Ram { get; set; }
+        public readonly double ScreenSize;
+        public string OS { get; set; }
+
+        public Computer(double ram, double screenSize, string oS)
+        {
+            Ram = ram;
+            ScreenSize = screenSize;
+            OS = oS;
+        }
+
+        public void PrintStats()
+        {
+            Console.WriteLine($"RAM: {Ram}{Environment.NewLine}" +
+                $"Screen Size: {ScreenSize}{Environment.NewLine}" +
+                $"OS: {OS}");
+        }
+
+        public void UpgradeRam(double uRam)
+        {
+            Ram += uRam;
+        }
+    }
+}
