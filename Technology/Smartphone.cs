c8257a2b8fe7849b7a4c1f5c1e3b729e685a2d6b
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-15 14:32:28 -0500

    dropped phone, broke camera

--- Technology/Smartphone.cs
+++ Technology/Smartphone.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Technology
 {
-    public class Smartphone
+    public class Smartphone : Computer
     {
+        public bool HasCamera { get; set; } = true;
+
+        public Smartphone(bool hasCamera, double ram, double screenSize, string oS) : base(ram, screenSize, oS)
+        {
+            HasCamera = hasCamera;
+        }
+
+        public void DropPhone()
+        {
+            HasCamera = false;
+        }
     }
 }
