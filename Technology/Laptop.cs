08d81c76a22c9535331c490a8fa050b15eb304b8
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-15 14:22:59 -0500

    stuffed some cheetos in a laptop

--- Technology/Laptop.cs
+++ Technology/Laptop.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Technology
 {
-    public class Laptop
+    public class Laptop : Computer
     {
+        public int NumOfUsb { get; set; }
+
+        public Laptop(int numOfUsb, double ram, double screenSize, string oS) : base(ram, screenSize, oS)
+        {
+            NumOfUsb = numOfUsb;
+        }
+
+        public void StuffedCheetos(int numOfCheetos)
+        {
+            NumOfUsb -= numOfCheetos;
+        }
     }
 }
